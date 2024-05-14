ea51fa4087d17f50ae590dd2fb067d2360fada20
maesterfox <talentedmrfox@hotmail.com>
2024-05-14 12:36:34 +0100

    Fix deployment configuration and static file serving

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,15 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import GridBackground from "./components/ui/GridBackground.jsx"; // corrected typo
+import GridBackground from "./components/ui/GridBackgroun.jsx";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-// Determine the GraphQL server URI based on the environment
 const client = new ApolloClient({
   uri:
     import.meta.env.VITE_NODE_ENV === "development"
       ? "http://localhost:4000/graphql"
-      : "https://expense-tracker-ten-green.vercel.app/", // update with your actual EC2 public DNS
+      : "http://ec2-13-50-240-13.eu-north-1.compute.amazonaws.com/graphql", // update with your actual EC2 public DNS
   cache: new InMemoryCache(),
   credentials: "include",
 });
