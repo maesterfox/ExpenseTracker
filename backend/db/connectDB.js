0ca1ebd77d6a90756e49575fbe829f53a96b1e20
maesterfox <talentedmrfox@hotmail.com>
2024-05-17 23:56:58 +0100

    Update MongoDB connection and refactor for deployment

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
+  if (!process.env.MONGO_URI) {
+    console.error("MONGO_URI is not set.");
+    process.exit(1);
+  }
+
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`Error connecting to db: ${err.message}`);
