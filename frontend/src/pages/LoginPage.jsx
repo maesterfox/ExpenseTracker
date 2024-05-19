ee212ab2a8ad643621fe5c7981a31feafb5f4df4
maesterfox <talentedmrfox@hotmail.com>
2024-05-19 12:13:07 +0100

    build

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -54,6 +54,7 @@ const LoginPage = () => {
                 value={loginData.username}
                 onChange={handleChange}
               />
+
               <InputField
                 label="Password"
                 id="password"
@@ -65,7 +66,8 @@ const LoginPage = () => {
               <div>
                 <button
                   type="submit"
-                  className="w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300 disabled:opacity-50 disabled:cursor-not-allowed"
+                  className="w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black  focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300 disabled:opacity-50 disabled:cursor-not-allowed
+									"
                   disabled={loading}
                 >
                   {loading ? "Loading..." : "Login"}
@@ -86,5 +88,4 @@ const LoginPage = () => {
     </div>
   );
 };
-
 export default LoginPage;
